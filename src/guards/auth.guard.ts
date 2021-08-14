e83b99003cee9e92bd6255bfbd426ff9f3f72993
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-08-14 03:19:03 -0700

    e2e test

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -3,7 +3,7 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const req = context.switchToHttp().getRequest();
-    console.log(req);
+
     return req.session.userId;
   }
 }
--- src/users/decorators/current-user.decorators.ts
+++ src/users/decorators/current-user.decorators.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const req = context.switchToHttp().getRequest(); //get all request
-    console.log(req.session.userId);
+
     return req.currentUser;
   },
 );
