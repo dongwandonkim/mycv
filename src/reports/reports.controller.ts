4939f855e352df0bc1a1b3cc55af06deecb49ad1
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-08-19 22:38:40 -0700

    adminGuard : authorization

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -15,6 +15,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 @Serialize(ReportDto)
@@ -28,6 +29,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
--- src/users/interceptors/current-user.interceptors.ts
+++ src/users/interceptors/current-user.interceptors.ts
@@ -12,7 +12,9 @@ export class CurrentUserInterceptor implements NestInterceptor {
 
   async intercept(context: ExecutionContext, handler: CallHandler) {
     const req = context.switchToHttp().getRequest();
+
     const { userId } = req.session;
+
     if (userId) {
       const user = await this.userService.findOne(userId);
       req.currentUser = user || {};
